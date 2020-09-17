ba91d5049771549c41eec709707bd72acb514861
jonathanx111 <jonathanx111@gmail.com>
2020-09-17 18:17:35 -0400

    fn

---
+++ lib/song.rb
@@ -0,0 +1,73 @@
+require 'pry'
+class Song
+    attr_accessor :name, :artist, :genre
+    @@count = 0
+    @@artists  = []
+    @@genres = []
+    
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@artists << artist
+        @@genres << genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        genre_count = {}
+        @@genres.each do |genre|
+            if genre_count[genre]
+                genre_count[genre] += 1 
+            else
+                genre_count[genre] = 1
+            end
+         end
+        genre_count
+    end
+
+    def self.artist_count
+        artist_hash = {}
+        
+        @@artists.each do |artist|
+            if artist_hash[artist]
+                artist_hash[artist] += 1
+            else
+                artist_hash[artist] = 1 
+            end
+        end
+        artist_hash
+    end
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+end
+
+["jayz", "jayz", "britany"]
